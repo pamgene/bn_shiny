f7333bd17b226f6b10e9fb8dbfa3f4a789af848b
Alexandre Maurel <alexandre.maurel@gmail.com>
2016-10-07 12:10:01 +0200

    add more validation on result

---
+++ R/validation.R
@@ -0,0 +1,57 @@
+
+#' @export
+validateResult = function(result){
+  if (inherits(result, "AnnotatedData")){
+    validateAnnotatedDataResult(result)
+  } else if (inherits(result, "data.frame")){
+    validateDataFrameResult(result)
+  } else {
+    stop("Result validation : wrong object type : result must be an AnnotatedData or a data.frame object")
+  }
+}
+
+#' @export
+validateAnnotatedDataResult = function(annotatedResult){
+  
+  data = annotatedResult$data
+  
+  # validate the data.frame
+  validateDataFrameResult(data)
+   
+  # ensure that qts are double
+  qtColNames = annotatedResult$qtColumnNames
+  
+  for (cname in qtColNames){
+    if (!is.double(data[[cname]])) stop(paste('Result validation : qt columns must be of type double :', cname))
+  }
+}
+
+#' @export
+validateDataFrameResult = function(df){
+  
+  # ensure that column are factor or character or double
+  cnames = colnames(df)
+  for (cname in cnames){
+    c = data[[cname]]
+    if (!(is.factor(c) || is.character(c) || is.double(c))){
+      stop(paste('Result validation : columns must be of type (factor | character | double) :',cname))
+    }
+  }
+   
+  if (!("colSeq" %in% colnames(df))){
+    stop('Result validation : column colSeq is required')
+  }
+  
+  if (!("rowSeq" %in% colnames(df))){
+    stop('Result validation : column rowSeq is required')
+  }
+  
+  
+  if (min(df[["colSeq"]]) < 1){
+    stop('Result validation : colSeq values must be greater or equals to 1')
+  }
+  if (min(df[["rowSeq"]]) < 1){
+    stop('Result validation : rowSeq values must be greater or equals to 1')
+  }
+}
+
